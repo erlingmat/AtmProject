27a8958d3d7ab980accbc15eb9b277c348be1146
Erling Matthiesen Møller <emm@ihp.dk>
2017-03-14 03:16:09 +0100

    Code

---
+++ AtmProject/AtmViewModel.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AtmProject
+{
+    public class AtmViewModel : ViewModelBase
+    {
+        private string _withdrawlamount = "";
+        private string _notes = "";
+        private string _bigcoins = "";
+        private string _smallcoins = "";
+        private MoneySorter sorter = new MoneySorter();
+
+       private ICommand _calculatewithdrawlcommand;
+       private ICommand _clearbinscommand;
+
+
+        public string WithDrawlAmount
+        {
+            get
+            {
+                return _withdrawlamount;
+            }
+
+            set
+            {
+                if (_withdrawlamount.Equals(value)) return;
+                _withdrawlamount = value;
+                OnPropertyChanged("WithDrawlAmount");
+            }
+        }
+
+        public string Notes
+        {
+            get { return _notes; }
+            set
+            {
+                if (_notes.Equals(value)) return;
+                _notes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string BigCoins
+        {
+            get { return _bigcoins; }
+            set
+            {
+                if (_bigcoins.Equals(value)) return;
+                _bigcoins = value;
+                OnPropertyChanged();
+            }
+           
+        }
+        public string SmallCoins
+        {
+            get { return _smallcoins; }
+            set
+            {
+                if (_smallcoins.Equals(value)) return;
+                _smallcoins = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AtmViewModel()
+        {
+            sorter.InitializeMoneyValues();
+            WithDrawlAmount = "0";
+            OnPropertyChanged();
+        }
+
+        public ICommand CalculateWithdrawlCommand
+        {
+            get
+            {
+ 
+                if (_calculatewithdrawlcommand == null)
+                    _calculatewithdrawlcommand = new RelayCommand(withdrawcommandfunction);
+                return _calculatewithdrawlcommand;
+            }
+
+        }
+
+
+        public ICommand ClearBinsCommand
+        {
+            get
+            {
+                
+                if (_clearbinscommand == null)
+                    _clearbinscommand = new RelayCommand(clearbinscommandfunction);
+                return _clearbinscommand;
+            }
+
+        }
+
+        private void clearbinscommandfunction(Object o)
+        {
+            Notes = "";
+            BigCoins = "";
+            SmallCoins = "";
+            WithDrawlAmount = "0";
+            OnPropertyChanged();
+        }
+
+        private string getCurrencyString(KeyValuePair<int, int> moneyPair)
+        {
+            return moneyPair.Key.ToString() + "Kr * " + moneyPair.Value.ToString() + "\n";
+        }
+
+        private void withdrawcommandfunction(Object o)
+        {
+            int withdrawlamount = int.Parse(WithDrawlAmount);
+            Dictionary<int, int> moneyamounts = sorter.CalculateMoneyAmounts(withdrawlamount);
+            foreach (var money in moneyamounts)
+            {
+                int bin = sorter.FindBin(money.Key);
+                if (bin == 0)
+                {
+                    Notes += getCurrencyString(money);
+                }
+                if (bin == 1)
+                {
+                    BigCoins += getCurrencyString(money);
+                }
+                if (bin == 2)
+                {
+                    SmallCoins += getCurrencyString(money);
+                }
+
+            }
+         OnPropertyChanged();
+        }
+
+    }
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        /// <summary>
+        /// Raised when a property on this object has a new value.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        #endregion // INotifyPropertyChanged Members
+    }
+
+    /// <summary>
+    /// A command whose sole purpose is to 
+    /// relay its functionality to other
+    /// objects by invoking delegates. The
+    /// default return value for the CanExecute
+    /// method is 'true'.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+       
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand( Action<object> execute,  Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// 
+        /// Convenient constructor shortcuts without action arguments.
+        /// 
+
+        public RelayCommand(Action execute,  Func<bool> canExecute) : this(obj => execute(), obj => canExecute == null || canExecute()) { }
+
+
+      
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+                try
+                {
+                    execute(parameter);
+                }
+                catch (Exception ex)
+                {
+                
+                }
+        }
+    }
+
+}
---
+++ AtmProject/MoneySorter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization.Advanced;
+
+namespace AtmProject
+{
+    public class MoneySorter
+    {
+        public Dictionary<int, int> MoneyValuesAndSizes = new Dictionary<int,int>();
+        // Notes are 1000 mm in diameter that is how they are distinguished from coins.
+        private int NoteSizeThreshold = 1000;
+        public List<int> MoneyValues = new List<int>();
+        private List<int> BinEdges = new List<int>();
+        private int NumberOfBins = 3;
+        /*
+- 1,000 note
+- 500 note
+- 200 note
+- 100 note
+- 50 note
+- 20 coin (40 mm)
+- 10 coin (20 mm)
+- 5 coin (50 mm)
+- 2 coin (30 mm)
+- 1 coin (10 mm)
+    */
+        public void InitializeMoneyValues()
+        {
+            BinEdges.Add(999);
+            BinEdges.Add(20);
+            MoneyValuesAndSizes.Add(1000, NoteSizeThreshold); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(500, NoteSizeThreshold); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(200, NoteSizeThreshold); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(100, NoteSizeThreshold); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(50, NoteSizeThreshold); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(20, 40); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(10, 20); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(5, 50); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(2, 30); // MoneyValue, Size
+            MoneyValuesAndSizes.Add(1, 10); // MoneyValue, Size
+            
+            MoneyValues.Add(1000);
+            MoneyValues.Add(500);
+            MoneyValues.Add(200);
+            MoneyValues.Add(100);
+            MoneyValues.Add(50);
+            MoneyValues.Add(20);
+            MoneyValues.Add(10);
+            MoneyValues.Add(5);
+            MoneyValues.Add(2);
+            MoneyValues.Add(1);
+
+
+        }
+
+        public int FindSize(int MoneyValue)
+        {
+            int size = 0;
+            if ( MoneyValuesAndSizes.TryGetValue(MoneyValue, out size))
+                return size;
+            return -1;
+        }
+
+        public int FindBin(int MoneyValue)
+        {
+            int TargetBin = -1;
+            int size = FindSize(MoneyValue);
+            if (size > 0)
+            {
+                if (size > BinEdges[0]) TargetBin = 0; // Note
+                if (size < BinEdges[0] && size > BinEdges[1]) TargetBin = 1;
+                if (size <= BinEdges[1]) TargetBin = 2;
+            }
+            return TargetBin;
+        }
+
+        public Dictionary<int, int> CalculateMoneyAmounts(int Withdrawl)
+        {
+            Dictionary<int, int> moneyamounts = new Dictionary<int, int>();
+            int rest = Withdrawl;
+            
+            foreach (var curMoneyValue in MoneyValues)
+            {
+                int count = 0;
+
+                while (rest - curMoneyValue >= 0)
+                {
+                    count++;
+                    rest -= curMoneyValue;
+                }
+                moneyamounts.Add(curMoneyValue, count);
+            }   
+            return moneyamounts;
+        } 
+    }
+}
